bbde693505e57976f9798ac484485d9584a8efb8
Dongxn@NUDT <Dong.Xiaoning@outlook.com>
2019-08-25 14:22:51 +0800

    implement whole python program instead of implementing some key functions in exercises

---
+++ Dictionary/word_frequency_in_files.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 10:56:53 2019
+
+@author: Dong Xiaoning
+"""
+
+
---
+++ Dictionary/word_frequency_in_one_file.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 10:54:54 2019
+
+@author: Dong Xiaoning
+"""
+
+# word frequency in a plain txt file
+
+import collections
+import operator
+
